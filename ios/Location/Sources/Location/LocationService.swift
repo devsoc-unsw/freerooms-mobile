b560d557e6af3be6740bf22d826de792a4695e3a
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-04-27 23:35:50 +1000

    feat: request permission implementation

--- ios/Location/Sources/Location/LocationService.swift
+++ ios/Location/Sources/Location/LocationService.swift
@@ -5,17 +5,57 @@
 //  Created by Anh Nguyen on 22/4/2025.
 //
 
+import CoreLocation
 import Foundation
 
-class LocationService {
+// MARK: - LocationService
+
+class LocationService: NSObject, LocationManagerDelegate {
+
+  // MARK: Lifecycle
+
+  init(locationManager: LocationManager) {
+    self.locationManager = locationManager
+    super.init()
+    self.locationManager.delegate = self
+  }
+
+  // MARK: Internal
+
+  var currentPermissionState = LocationPermission.unrequested
+
   func getCurrentLocation() throws -> Location {
     fatalError("TODO: Implement")
   }
-  
+
   func requestLocationPermissions() -> Bool {
-    fatalError("TODO: Implement")
+    if currentPermissionState == .pending {
+      return false
+    }
+
+    currentPermissionState = .pending
+    locationManager.requestWhenInUseAuthorization()
+    return true
   }
+
+  func locationManagerDidChangeAuthorization(_: LocationManager) {
+    switch locationManager.authorizationStatus {
+    case .authorizedWhenInUse:
+      currentPermissionState = .granted
+    case .denied:
+      currentPermissionState = .denied
+    default:
+      currentPermissionState = .unrequested
+    }
+  }
+
+  // MARK: Private
+
+  private var locationManager: LocationManager
+
 }
+
+// MARK: - LocationServiceError
 
 enum LocationServiceError: Error {
   case locationPermissionsDenied
