fda505a52e41d521ab80366b6f0daebd502bbf15
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-04-27 19:44:09 +1000

    test: add mock location manager

--- ios/Location/Tests/LocationTests/LocationTests.swift
+++ ios/Location/Tests/LocationTests/LocationTests.swift
@@ -1,6 +1,47 @@
 import Testing
+import CoreLocation
 @testable import Location
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test
+func request_location_permission_denial() async throws {
+  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+@Test
+func request_location_permission_granted() async throws {
+  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+
+// MARK: - LocationManager Protocol
+
+protocol LocationManager {
+  // MARK: Internal
+  var delegate: CLLocationManagerDelegate? { get set }
+  var authorizationStatus: CLAuthorizationStatus { get }
+  
+  func requestWhenInUseAuthorization()
+}
+
+// MARK: - MockLocationManager
+
+class MockLocationManager: LocationManager {
+  var requestInUseAuthorizationCallTracker = 0;
+  var delegate: CLLocationManagerDelegate?
+  
+  // MARK: Mock AuthorizationStatus field
+  private var _authorizationStatus =  CLAuthorizationStatus.notDetermined
+  var authorizationStatus: CLAuthorizationStatus {
+    return _authorizationStatus
+  }
+
+  func requestWhenInUseAuthorization() {
+    requestInUseAuthorizationCallTracker += 1
+  }
+  
+  // MARK: to mock the authorization status
+  func simulateAuthorizationStatus(to status: CLAuthorizationStatus) {
+    _authorizationStatus = status
+  }
+  
 }
