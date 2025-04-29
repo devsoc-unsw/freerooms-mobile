b0980bae501487f2c64d1af56463fe7507bc5c78
Elvene <123799877+xleonx0x@users.noreply.github.com>
2025-04-29 23:10:55 +1000

    feat: implement fetch buildings in building service

--- ios/Buildings/Sources/Buildings/BuildingService.swift
+++ ios/Buildings/Sources/Buildings/BuildingService.swift
@@ -6,9 +6,34 @@
 //
 
 import Foundation
+import Networking
+
+// MARK: - BuildingService
 
 class BuildingService {
-  func getBuildings() -> [Building] {
-    fatalError("TODO: Implement")
+
+  // MARK: Lifecycle
+
+  init(buildingLoader: any BuildingLoader) {
+    self.buildingLoader = buildingLoader
   }
+
+  // MARK: Public
+
+  public typealias Result = Swift.Result<[RemoteBuilding], Swift.Error>
+
+  // MARK: Internal
+
+  func getBuildings() async -> Result {
+    switch await buildingLoader.fetch() {
+    case .success(let buildings):
+      .success(buildings)
+    case .failure(let error):
+      .failure(error)
+    }
+  }
+
+  // MARK: Private
+
+  private var buildingLoader: any BuildingLoader
 }
