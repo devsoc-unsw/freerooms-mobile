8aedb230583ea8e35e99ac579f879633cadeb1ac
Elvene <123799877+xleonx0x@users.noreply.github.com>
2025-04-29 23:10:30 +1000

    chore: add protocol for building loader

--- ios/Buildings/Sources/Buildings/BuildingLoader.swift
+++ ios/Buildings/Sources/Buildings/BuildingLoader.swift
@@ -5,6 +5,8 @@
 //  Created by Chris Wong on 29/4/2025.
 //
 
+import Networking
+
 public protocol BuildingLoader: CodableLoader {
-  func fetch() async -> Result<[Building], Error>
+  func fetch() async -> Result<[RemoteBuilding], Error>
 }
