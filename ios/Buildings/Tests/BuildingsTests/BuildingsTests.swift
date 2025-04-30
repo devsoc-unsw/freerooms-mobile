83cdb467be31b85dd09a7f094d6e1cc50f1edbc7
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-04-30 15:01:05 +1000

    tests: add tests for gridReference Computed properties

--- ios/Buildings/Tests/BuildingsTests/BuildingsTests.swift
+++ ios/Buildings/Tests/BuildingsTests/BuildingsTests.swift
@@ -1,7 +1,75 @@
 import Testing
 @testable import Buildings
 
-@Test
-func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+// MARK: - GetBuildingByCampusSectionTest
+
+struct GetBuildingByCampusSectionTest {
+  @Test
+  func sortByAscending() {
+    // TODO
+  }
+
+  @Test
+  func sortByDescending() {
+    // TODO
+  }
+
+}
+
+// MARK: - GridReferenceTest
+
+struct GridReferenceTest {
+  @Test
+  func upperCampusBuildingTest() {
+    let building = Building(
+      name: "Patricia O Shane",
+      id: "K-E19",
+      latitude: -33.917177,
+      longitude: 151.232511,
+      aliases: ["Central Lecture Block"],
+      numberOfAvailableRooms: 0)
+
+    let sut = building.gridReference!
+
+    #expect(sut.campusCode == "K")
+    #expect(sut.sectionCode == "E")
+    #expect(sut.sectionNumber == 19)
+    #expect(sut.campusSection == CampusSection.upper)
+  }
+
+  @Test
+  func middleCampusBuildingTest() {
+    let building = Building(
+      name: "Quadrangle",
+      id: "K-E15",
+      latitude: 0.0,
+      longitude: 0.0,
+      aliases: ["Quad"],
+      numberOfAvailableRooms: 0)
+
+    let sut = building.gridReference!
+
+    #expect(sut.campusCode == "K")
+    #expect(sut.sectionCode == "E")
+    #expect(sut.sectionNumber == 15)
+    #expect(sut.campusSection == CampusSection.middle)
+  }
+
+  @Test
+  func lowerCampusBuildingTest() {
+    let building = Building(
+      name: "Law Library",
+      id: "K-F8",
+      latitude: 0.0,
+      longitude: 0.0,
+      aliases: ["Law Library"],
+      numberOfAvailableRooms: 0)
+
+    let sut = building.gridReference!
+
+    #expect(sut.campusCode == "K")
+    #expect(sut.sectionCode == "F")
+    #expect(sut.sectionNumber == 8)
+    #expect(sut.campusSection == CampusSection.lower)
+  }
 }
