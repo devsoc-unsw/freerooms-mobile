35a9e7c5b6957ba97b50a156cbb57ff6269c4df0
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-05-01 00:23:59 +1000

    feat: add get building sorted by campus section method

--- ios/Buildings/Sources/Buildings/BuildingInteractor.swift
+++ ios/Buildings/Sources/Buildings/BuildingInteractor.swift
@@ -8,15 +8,41 @@
 import Foundation
 
 final class BuildingInteractor {
+
+  // MARK: Lifecycle
+
+  init(buildingService: BuildingServiceProtocol) {
+    self.buildingService = buildingService
+  }
+
+  // MARK: Internal
+
   func getBuildingsSortedByAvailableRooms() -> [Building] {
     fatalError("TODO: Implement")
   }
-  
-  func getBuildingsSortedAlphabetically(inAscendingOrder isAscending: Bool) -> [Building] {
+
+  func getBuildingsSortedAlphabetically(inAscendingOrder _: Bool) -> [Building] {
     fatalError("TODO: Implement")
   }
-  
-  func getBuildingsSortedByDistance(inAscendingOrder isAscending: Bool) -> [Building] {
+
+  func getBuildingsSortedByDistance(inAscendingOrder _: Bool) -> [Building] {
     fatalError("TODO: Implement")
   }
+
+  func getBuildingSortedByCampusSection(inAscendingOrder: Bool) -> [Building] {
+    var buildings = buildingService.getBuildings()
+
+    if inAscendingOrder {
+      buildings = buildings.sorted { $0.gridReference!.campusSection < $1.gridReference!.campusSection }
+    } else {
+      buildings = buildings.sorted { $0.gridReference!.campusSection > $1.gridReference!.campusSection }
+    }
+
+    return buildings
+  }
+
+  // MARK: Private
+
+  private let buildingService: BuildingServiceProtocol
+
 }
