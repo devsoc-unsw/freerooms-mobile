ed568958dac1d2b1d9a5b2f67f94300e29235fb2
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-05-15 14:08:01 +1000

    chore: remove unused code

--- ios/Location/Tests/LocationTests/LocationTests.swift
+++ ios/Location/Tests/LocationTests/LocationTests.swift
@@ -121,33 +121,3 @@ struct LocationServicePermissionTest {
   }
 
 }
-
-// MARK: - MockLocationManager
-//
-// class MockLocationManager: LocationManager {
-//
-//  // MARK: Internal
-//
-//  var requestInUseAuthorizationCallTracker = 0
-//  var delegate: LocationManagerDelegate?
-//
-//  var authorizationStatus: CLAuthorizationStatus {
-//    _authorizationStatus
-//  }
-//
-//  func requestWhenInUseAuthorization() {
-//    requestInUseAuthorizationCallTracker += 1
-//  }
-//
-//  // MARK: to mock the authorization status
-//  func simulateAuthorizationStatus(to status: CLAuthorizationStatus) {
-//    _authorizationStatus = status
-//    delegate?.locationManagerDidChangeAuthorization?(self)
-//  }
-//
-//  // MARK: Private
-//
-//  // MARK: Mock AuthorizationStatus field
-//  private var _authorizationStatus = CLAuthorizationStatus.notDetermined
-//
-// }
