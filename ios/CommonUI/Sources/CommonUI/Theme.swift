42ed548e683e967c2d6857831fd3869c86d57506
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-06-13 16:01:51 +1000

    feat(ui): add additional colours for theming

--- ios/CommonUI/Sources/CommonUI/Theme.swift
+++ ios/CommonUI/Sources/CommonUI/Theme.swift
@@ -16,31 +16,68 @@ import UIKit
 @Observable
 @MainActor
 public final class Theme {
-
+  
   // MARK: Lifecycle
-
-  public init(accent: Accent) {
+  
+  public init(accent: Accent, label: Label, background: Color, toolbar: Color, yellow: Color, white: Color) {
     self.accent = accent
+    self.label = label
+    self.background = background
+    self.toolbar = toolbar
+    self.yellow = yellow
+    self.white = white
   }
-
+  
   // MARK: Public
-
+  
   public struct Accent {
     public let primary: Color
     public let secondary: Color
-
-    public init(primary: Color, secondary: Color) {
+    public let tertiary: Color
+    public let quaternary: Color
+    
+    public init(primary: Color, secondary: Color, tertiary: Color, quaternary: Color) {
       self.primary = primary
       self.secondary = secondary
+      self.tertiary = tertiary
+      self.quaternary = quaternary
     }
   }
-
-  public static let light = Theme(accent: .init(
-    primary: Color("AccentPrimary", bundle: .module),
-    secondary: Color("AccentSecondary", bundle: .module)))
-
+  
+  public struct Label {
+    public let primary: Color
+    public let secondary: Color
+    public let tertiary: Color
+    
+    public init(primary: Color, secondary: Color, tertiary: Color) {
+      self.primary = primary
+      self.secondary = secondary
+      self.tertiary = tertiary
+    }
+  }
+  
+  public static let light = Theme(
+    accent: .init(
+      primary: Color("AccentPrimary", bundle: .module),
+      secondary: Color("AccentSecondary", bundle: .module),
+      tertiary: Color("AccentTertiary", bundle: .module),
+      quaternary: Color("AccentQuarternary", bundle: .module)),
+    label: .init(
+      primary: Color("LabelPrimary", bundle: .module),
+      secondary: Color("LabelSecondary", bundle: .module),
+      tertiary: Color("LabelTertiary", bundle: .module)),
+    background: .init("Background", bundle: .module),
+    toolbar: .init("Toolbar", bundle: .module),
+    yellow: .init("Yellow", bundle: .module),
+    white: .init("White", bundle: .module))
+  
   public var accent: Accent
-
+  public var label: Label
+  public var background: Color
+  public var toolbar: Color
+  public var yellow: Color
+  public var white: Color
+  
   /// DO NOT CALL IN PREVIEWS
   public static func registerFont(named name: String) {
     guard let asset = NSDataAsset(name: "Fonts/\(name)", bundle: Bundle.module) else {
