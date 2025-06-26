c5c8e6530257fc60d420436561459af441c99cac
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2025-06-26 21:14:02 +1000

    chore: fixed tests using not using array type

--- ios/Persistence/Tests/PersistenceTests/JSONLoaderTests.swift
+++ ios/Persistence/Tests/PersistenceTests/JSONLoaderTests.swift
@@ -16,14 +16,14 @@ struct JSONLoaderTests {
   @Test("JSON loader decodes json file into platform type [PlatformBuildings]")
   func loadsBuildingsFromJSONFile() {
     // Given
-    let mockFileLoader = MockFileLoader(loads: "Data")
-    let sut = LiveJSONLoader<PlatformBuilding>(decodes: "buildings", with: mockFileLoader)
+    let mockFileLoader = MockFileLoader(loads: fakeBuildingsJSON)
+    let sut = LiveJSONLoader<[PlatformBuilding]>(decodes: "buildings.json", with: mockFileLoader)
 
     // When
     let res = sut.load(from: "fakeFilePath/fake")
 
     // Then
-    let platformBuildings = [PlatformBuilding]()
+    let platformBuildings = createPlatformBuildings(2)
     expect(res, toFetch: platformBuildings)
   }
 
@@ -31,7 +31,7 @@ struct JSONLoaderTests {
   func throwsErrorOnFileNotExisting() {
     // Given
     let mockFileLoader = MockFileLoader(throws: FileLoaderError.fileNotFound)
-    let sut = LiveJSONLoader<PlatformBuilding>(decodes: "buildings", with: mockFileLoader)
+    let sut = LiveJSONLoader<[PlatformBuilding]>(decodes: "buildings.json", with: mockFileLoader)
 
     // When
     let res = sut.load(from: "fakeFilePath/fake")
@@ -43,8 +43,8 @@ struct JSONLoaderTests {
   @Test("Throws an error when JSON in file is malformed")
   func throwsErrorOnMalformedJSON() {
     // Given
-    let mockFileLoader = MockFileLoader(loads: "Malformed data")
-    let sut = LiveJSONLoader<PlatformBuilding>(decodes: "buildings", with: mockFileLoader)
+    let mockFileLoader = MockFileLoader(loads: fakeBuildingsMalformedJSON)
+    let sut = LiveJSONLoader<[PlatformBuilding]>(decodes: "buildings.json", with: mockFileLoader)
 
     // When
     let res = sut.load(from: "fakeFilePath/fake")
@@ -55,7 +55,45 @@ struct JSONLoaderTests {
 
   // MARK: Private
 
-  private func expect(_ res: LiveJSONLoader<PlatformBuilding>.Result, toThrow _: LiveJSONLoaderError) {
+  private let fakeBuildingsJSON = """
+    [
+          {
+            "aliases": ["A", "B"],
+            "id": "123",
+            "lat": 1.0,
+            "name": "name",
+            "long": 1.0
+          },
+          {
+            "aliases": ["A", "B"],
+            "id": "123",
+            "lat": 1.0,
+            "name": "name",
+            "long": 1.0
+          }
+    ]
+    """
+
+  private let fakeBuildingsMalformedJSON = """
+     "buil [
+          {
+            "aliases": ["A", "B"],
+            "id": "123",
+            "lat": 1.0,
+            "name": "name",
+            "long": 1.0
+          },
+          {
+            "aliases": ["A", "B"],
+            "id": "123",
+            "lat": 1.0,
+            "name": "name",
+            "long": 1.0
+          }
+    ]
+    """
+
+  private func expect(_ res: LiveJSONLoader<[PlatformBuilding]>.Result, toThrow _: LiveJSONLoaderError) {
     switch res {
     case .failure(let error):
       #expect(error == error)
@@ -64,13 +102,21 @@ struct JSONLoaderTests {
     }
   }
 
-  private func expect(_ res: LiveJSONLoader<PlatformBuilding>.Result, toFetch _: [PlatformBuilding]) {
+  private func expect(_ res: LiveJSONLoader<[PlatformBuilding]>.Result, toFetch _: [PlatformBuilding]) {
     switch res {
     case .success(let buildings):
       #expect(buildings == buildings)
     case .failure(let error):
       Issue.record("Expected success but got \(error)")
     }
+  }
+
+  private func createPlatformBuildings(_ count: Int) -> [PlatformBuilding] {
+    var platformBuildings: [PlatformBuilding] = []
+    for _ in 0..<count {
+      platformBuildings.append(PlatformBuilding(name: "name", id: "123", lat: 1.0, long: 1.0, aliases: ["A", "B"]))
+    }
+    return platformBuildings
   }
 
 }
