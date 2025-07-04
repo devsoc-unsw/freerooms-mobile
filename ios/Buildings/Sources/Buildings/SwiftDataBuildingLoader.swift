43bf6f02c440fac9fbc03ba596d5a7e49f214981
Muqueet Mohsen Chowdhury <mmc@berkeley.edu>
2025-07-04 13:34:32 +1000

    make SwiftDataBuildingLoader accept any PersistentStore for testing

--- ios/Buildings/Sources/Buildings/SwiftDataBuildingLoader.swift
+++ ios/Buildings/Sources/Buildings/SwiftDataBuildingLoader.swift
@@ -8,12 +8,14 @@
 import Foundation
 import Persistence
 
+// MARK: - SwiftDataBuildingLoader
+
 public final class SwiftDataBuildingLoader: BuildingLoader {
 
   // MARK: Lifecycle
 
-  public init(swiftDataStore: SwiftDataStore<SwiftDataBuilding>) {
-    self.swiftDataStore = swiftDataStore
+  public init<Store: PersistentStore>(swiftDataStore: Store) where Store.Model == SwiftDataBuilding {
+    self.swiftDataStore = AnyPersistentStore(swiftDataStore)
   }
 
   // MARK: Public
@@ -36,5 +38,57 @@ public final class SwiftDataBuildingLoader: BuildingLoader {
 
   // MARK: Private
 
-  private let swiftDataStore: SwiftDataStore<SwiftDataBuilding>
+  private let swiftDataStore: AnyPersistentStore<SwiftDataBuilding>
+}
+
+// MARK: - AnyPersistentStore
+
+private struct AnyPersistentStore<Model>: PersistentStore {
+
+  // MARK: Lifecycle
+
+  init<Store: PersistentStore>(_ store: Store) where Store.Model == Model {
+    _save = store.save
+    _saveMultiple = store.save
+    _fetchAll = store.fetchAll
+    _fetch = store.fetch
+    _delete = store.delete
+    _deleteMultiple = store.delete
+  }
+
+  // MARK: Internal
+
+  func save(_ item: Model) throws {
+    try _save(item)
+  }
+
+  func save(_ items: [Model]) throws {
+    try _saveMultiple(items)
+  }
+
+  func fetchAll() throws -> [Model] {
+    try _fetchAll()
+  }
+
+  func fetch(id: String) throws -> Model? {
+    try _fetch(id)
+  }
+
+  func delete(_ item: Model) throws {
+    try _delete(item)
+  }
+
+  func delete(_ items: [Model]) throws {
+    try _deleteMultiple(items)
+  }
+
+  // MARK: Private
+
+  private let _save: (Model) throws -> Void
+  private let _saveMultiple: ([Model]) throws -> Void
+  private let _fetchAll: () throws -> [Model]
+  private let _fetch: (String) throws -> Model?
+  private let _delete: (Model) throws -> Void
+  private let _deleteMultiple: ([Model]) throws -> Void
+
 }
