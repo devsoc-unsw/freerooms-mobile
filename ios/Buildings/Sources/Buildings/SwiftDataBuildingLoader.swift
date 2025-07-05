f0714b4dbd6e84c7d81c2eb24349701ffac8e908
Muqueet Mohsen Chowdhury <mmc@berkeley.edu>
2025-07-05 11:00:43 +1000

    use primary associated types to eliminate type erasure boilerplate

--- ios/Buildings/Sources/Buildings/SwiftDataBuildingLoader.swift
+++ ios/Buildings/Sources/Buildings/SwiftDataBuildingLoader.swift
@@ -14,8 +14,8 @@ public final class SwiftDataBuildingLoader: BuildingLoader {
 
   // MARK: Lifecycle
 
-  public init<Store: PersistentStore>(swiftDataStore: Store) where Store.Model == SwiftDataBuilding {
-    self.swiftDataStore = AnyPersistentStore(swiftDataStore)
+  public init(swiftDataStore: some PersistentStore<SwiftDataBuilding>) {
+    self.swiftDataStore = swiftDataStore
   }
 
   // MARK: Public
@@ -38,57 +38,5 @@ public final class SwiftDataBuildingLoader: BuildingLoader {
 
   // MARK: Private
 
-  private let swiftDataStore: AnyPersistentStore<SwiftDataBuilding>
-}
-
-// MARK: - AnyPersistentStore
-
-private struct AnyPersistentStore<Model>: PersistentStore {
-
-  // MARK: Lifecycle
-
-  init<Store: PersistentStore>(_ store: Store) where Store.Model == Model {
-    _save = store.save
-    _saveMultiple = store.save
-    _fetchAll = store.fetchAll
-    _fetch = store.fetch
-    _delete = store.delete
-    _deleteMultiple = store.delete
-  }
-
-  // MARK: Internal
-
-  func save(_ item: Model) throws {
-    try _save(item)
-  }
-
-  func save(_ items: [Model]) throws {
-    try _saveMultiple(items)
-  }
-
-  func fetchAll() throws -> [Model] {
-    try _fetchAll()
-  }
-
-  func fetch(id: String) throws -> Model? {
-    try _fetch(id)
-  }
-
-  func delete(_ item: Model) throws {
-    try _delete(item)
-  }
-
-  func delete(_ items: [Model]) throws {
-    try _deleteMultiple(items)
-  }
-
-  // MARK: Private
-
-  private let _save: (Model) throws -> Void
-  private let _saveMultiple: ([Model]) throws -> Void
-  private let _fetchAll: () throws -> [Model]
-  private let _fetch: (String) throws -> Model?
-  private let _delete: (Model) throws -> Void
-  private let _deleteMultiple: ([Model]) throws -> Void
-
+  private let swiftDataStore: any PersistentStore<SwiftDataBuilding>
 }
