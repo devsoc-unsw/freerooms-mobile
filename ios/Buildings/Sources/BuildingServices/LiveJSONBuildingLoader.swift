2c2673f867605b48e708e1329f29fa9d181632d6
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2025-08-14 08:44:45 +1000

    chore: made computed component for buildings seed json path

--- ios/Buildings/Sources/BuildingServices/LiveJSONBuildingLoader.swift
+++ ios/Buildings/Sources/BuildingServices/LiveJSONBuildingLoader.swift
@@ -6,13 +6,23 @@
 //
 
 import BuildingModels
+import Foundation
 import Persistence
 
-public struct LiveJSONBuildingLoader: BuildingLoader {
+// MARK: - JSONBuildingLoader
+
+public protocol JSONBuildingLoader {
+  func decode(from filename: String) -> Swift.Result<[Building], BuildingLoaderError>
+  func fetch() -> Swift.Result<[Building], BuildingLoaderError>
+}
+
+// MARK: - LiveJSONBuildingLoader
+
+public struct LiveJSONBuildingLoader: JSONBuildingLoader {
 
   // MARK: Lifecycle
 
-  public init(using loader: any JSONLoader<[DecodableBuilding]>) {
+  public init(using loader: any JSONLoader<DecodableBuildingData>) {
     self.loader = loader
   }
 
@@ -20,10 +30,14 @@ public struct LiveJSONBuildingLoader: BuildingLoader {
 
   public typealias Result = Swift.Result<[Building], BuildingLoaderError>
 
+  public var buildingsSeedJSONPath: String? {
+    Bundle.module.path(forResource: "BuildingsSeed", ofType: "json")
+  }
+
   public func decode(from filename: String) -> Result {
     switch loader.load(from: filename) {
     case .success(let decodedBuildings):
-      let buildings = decodedBuildings.map {
+      let buildings = decodedBuildings.data.buildings.map {
         Building(name: $0.name, id: $0.id, latitude: $0.lat, longitude: $0.long, aliases: $0.aliases)
       }
       return .success(buildings)
@@ -37,9 +51,13 @@ public struct LiveJSONBuildingLoader: BuildingLoader {
   }
 
   public func fetch() -> Result {
-    switch loader.load(from: "/path") {
-    case .success(let decodedBuildings):
-      let buildings = decodedBuildings.map {
+    guard let buildingsSeedJSONPath else {
+      fatalError("No JSON Buildings Seed bundled")
+    }
+
+    switch loader.load(from: buildingsSeedJSONPath) {
+    case .success(let JSONBuildingsResponse):
+      let buildings = JSONBuildingsResponse.data.buildings.map {
         Building(name: $0.name, id: $0.id, latitude: $0.lat, longitude: $0.long, aliases: $0.aliases)
       }
       return .success(buildings)
@@ -54,6 +72,6 @@ public struct LiveJSONBuildingLoader: BuildingLoader {
 
   // MARK: Private
 
-  private let loader: any JSONLoader<[DecodableBuilding]>
+  private let loader: any JSONLoader<DecodableBuildingData>
 
 }
