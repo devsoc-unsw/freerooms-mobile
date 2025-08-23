829d809af475ad48e305fdef6ff24564bd1eb706
Yanlin Li <yanauslin8@gmail.com>
2025-08-24 02:06:11 +1000

    feat: use live implementation part 2 and matching the building tab ui to the figma design more closely

--- ios/Buildings/Sources/BuildingViews/BuildingsTabView.swift
+++ ios/Buildings/Sources/BuildingViews/BuildingsTabView.swift
@@ -34,6 +34,40 @@ public struct BuildingsTabView: View {
         buildingsView(for: "Upper campus", from: viewModel.upperCampusBuildings)
 
         buildingsView(for: "Middle campus", from: viewModel.middleCampusBuildings)
+
+        buildingsView(for: "Lower campus", from: viewModel.lowerCampusBuildings)
+      }
+      .toolbar {
+        // Buttons on the right
+        ToolbarItemGroup(placement: .navigationBarTrailing) {
+          HStack {
+            Button {
+              // action
+            } label: {
+              Image(systemName: "line.3.horizontal.decrease")
+                .resizable()
+                .frame(width: 22, height: 15)
+            }
+
+            Button {
+              viewModel.getBuildingsInOrder()
+            } label: {
+              Image(systemName: "arrow.up.arrow.down")
+                .resizable()
+                .frame(width: 25, height: 20)
+            }
+
+            Button {
+              // action
+            } label: {
+              Image(systemName: "list.bullet")
+                .resizable()
+                .frame(width: 22, height: 15)
+            }
+          }
+          .padding(.trailing, 10)
+          .foregroundStyle(theme.label.tertiary)
+        }
       }
       .listRowInsets(EdgeInsets()) // Removes the large default padding around a list
       .scrollContentBackground(.hidden) // Hides default grey background of the list to allow shadow to appear correctly under section cards
@@ -60,12 +94,13 @@ public struct BuildingsTabView: View {
           : 1) // This hides a glitch where the bottom border of top section row and vice versa flashes when changing order
         .onAppear(perform: viewModel.onAppear)
         .navigationTitle("Buildings")
+        .searchable(text: $searchText, prompt: "Search...")
     }
-    .overlay(alignment: .bottom) {
-      Button("Buildings in ascending order: \(viewModel.buildingsInAscendingOrder)", action: viewModel.getBuildingsInOrder)
-        .buttonStyle(.borderedProminent)
-        .padding(.bottom)
-    }
+//    .overlay(alignment: .bottom) {
+//      Button("Buildings in ascending order: \(viewModel.buildingsInAscendingOrder)", action: viewModel.getBuildingsInOrder)
+//        .buttonStyle(.borderedProminent)
+//        .padding(.bottom)
+//    }
     .tabItem {
       Label("Buildings", systemImage: "building")
     }
@@ -78,6 +113,8 @@ public struct BuildingsTabView: View {
 
   @State var path = NavigationPath()
   @State var rowHeight: CGFloat?
+
+  @State var searchText = ""
 
   func buildingsView(for campus: String, from buildings: [Building]) -> some View {
     Section {
