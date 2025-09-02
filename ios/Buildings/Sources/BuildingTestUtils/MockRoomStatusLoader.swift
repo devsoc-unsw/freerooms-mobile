b5e77fecc0e4c6338629acdc754c697cc554bb89
Muqueet Mohsen Chowdhury <mmc@berkeley.edu>
2025-09-02 10:24:06 +0600

    feat: add MockRoomStatusLoader for testing room status functionality

---
+++ ios/Buildings/Sources/BuildingTestUtils/MockRoomStatusLoader.swift
@@ -0,0 +1,48 @@
+//
+//  MockRoomStatusLoader.swift
+//  BuildingTestUtils
+//
+//  Created by Muqueet Mohsen Chowdhury on 2/9/2025.
+//
+
+import BuildingModels
+import BuildingServices
+import Foundation
+
+// MARK: - MockRoomStatusLoader
+
+public final class MockRoomStatusLoader: RoomStatusLoader {
+
+  // MARK: Lifecycle
+
+  public init() { }
+
+  // MARK: Public
+
+  public func fetchRoomStatus() async -> Result<RoomStatusResponse, RoomStatusLoaderError> {
+    if let stubbedError {
+      return .failure(stubbedError)
+    }
+
+    if let stubbedResponse {
+      return .success(stubbedResponse)
+    }
+
+    return .failure(.connectivity)
+  }
+
+  public func stubSuccess(_ response: RoomStatusResponse) {
+    stubbedResponse = response
+    stubbedError = nil
+  }
+
+  public func stubFailure(_ error: RoomStatusLoaderError = .connectivity) {
+    stubbedError = error
+    stubbedResponse = nil
+  }
+
+  // MARK: Private
+
+  private var stubbedResponse: RoomStatusResponse?
+  private var stubbedError: RoomStatusLoaderError?
+}
