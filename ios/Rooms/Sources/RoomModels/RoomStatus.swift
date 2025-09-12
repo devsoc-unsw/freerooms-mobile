332b6982225012cee5dda7ad5e38be43f636ef18
Yanlin Li <yanauslin8@gmail.com>
2025-09-12 18:20:43 +1000

    "feat: resolve merge conflict with RoomStatus"

---
+++ ios/Rooms/Sources/RoomModels/RoomStatus.swift
@@ -0,0 +1,39 @@
+//
+//  RemoteRoomStatus.swift
+//  Rooms
+//
+//  Created by Muqueet Mohsen Chowdhury on 2/9/2025.
+//
+
+import Foundation
+
+// MARK: - RoomStatus
+
+/// Represents the status of an individual room
+public struct RoomStatus: Codable, Equatable {
+  public let status: String
+  public let endtime: String
+
+  public init(status: String, endtime: String) {
+    self.status = status
+    self.endtime = endtime
+  }
+}
+
+// MARK: - RemoteRoomStatus
+
+/// Response model for the /api/rooms/status endpoint
+public typealias RemoteRoomStatus = [String: BuildingRoomStatus]
+
+// MARK: - BuildingRoomStatus
+
+/// Represents room status data for a specific building
+public struct BuildingRoomStatus: Codable, Equatable {
+  public let numAvailable: Int
+  public let roomStatuses: [String: RoomStatus]
+
+  public init(numAvailable: Int, roomStatuses: [String: RoomStatus]) {
+    self.numAvailable = numAvailable
+    self.roomStatuses = roomStatuses
+  }
+}
