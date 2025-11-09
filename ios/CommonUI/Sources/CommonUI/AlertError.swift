342c3865d0dc010fa725e70ebd840b765c3f071c
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-11-09 16:18:47 +1100

    feat: add AlertError for error handling

---
+++ ios/CommonUI/Sources/CommonUI/AlertError.swift
@@ -0,0 +1,18 @@
+//
+//  AlertError.swift
+//  CommonUI
+//
+//  Created by Dicko Evaldo on 25/10/2025.
+//
+import Foundation
+
+public struct AlertError: Identifiable {
+  public let id = UUID()
+  public let title: String
+  public let message: String
+
+  public init(title: String = "Error", message: String) {
+    self.title = title
+    self.message = message
+  }
+}
