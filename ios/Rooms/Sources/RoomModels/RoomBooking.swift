4a9fe30c46bc2808f225d780ca2fba2688d60156
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2025-11-12 17:49:50 +1100

    chore: fix formatter

--- ios/Rooms/Sources/RoomModels/RoomBooking.swift
+++ ios/Rooms/Sources/RoomModels/RoomBooking.swift
@@ -19,30 +19,36 @@ public struct RoomBooking: Equatable, Sendable, Hashable {
   }
 
   // MARK: Public
+  
+  private static let formatter: ISO8601DateFormatter = {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    return formatter
+  }()
 
   public static let exampleOne = RoomBooking(
       bookingType: "CLASS",
-      end: ISO8601DateFormatter().date(from: "2025-11-11T23:30:00.000Z")!,
+      end: formatter.date(from: "2025-11-11T23:30:00.000Z")!,
       name: "COMM2501 LAB",
-      start: ISO8601DateFormatter().date(from: "2025-11-11T22:00:00.000Z")!)
+      start: formatter.date(from: "2025-11-11T22:00:00.000Z")!)
 
   public static let exampleTwo = RoomBooking(
       bookingType: "CLASS",
-      end: ISO8601DateFormatter().date(from: "2025-11-12T01:00:00.000Z")!,
+      end: formatter.date(from: "2025-11-12T01:00:00.000Z")!,
       name: "COMM2501 LAB",
-      start: ISO8601DateFormatter().date(from: "2025-11-11T23:30:00.000Z")!)
+      start: formatter.date(from: "2025-11-11T23:30:00.000Z")!)
 
   public static let exampleThree = RoomBooking(
       bookingType: "CLASS",
-      end: ISO8601DateFormatter().date(from: "2025-11-12T02:30:00.000Z")!,
+      end: formatter.date(from: "2025-11-12T02:30:00.000Z")!,
       name: "COMM2501 LAB",
-      start: ISO8601DateFormatter().date(from: "2025-11-12T01:00:00.000Z")!)
+      start: formatter.date(from: "2025-11-12T01:00:00.000Z")!)
 
   public static let exampleFour = RoomBooking(
       bookingType: "CLASS",
-      end: ISO8601DateFormatter().date(from: "2025-11-12T04:00:00.000Z")!,
+      end: formatter.date(from: "2025-11-12T04:00:00.000Z")!,
       name: "COMM2501 LAB",
-      start: ISO8601DateFormatter().date(from: "2025-11-12T02:30:00.000Z")!)
+      start: formatter.date(from: "2025-11-12T02:30:00.000Z")!)
   
   
   public let bookingType: String
