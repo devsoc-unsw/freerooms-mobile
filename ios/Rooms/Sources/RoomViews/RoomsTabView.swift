2a0150f9c0f826b4ee9a82f748a0573066fa1193
Yanlin Li <yanauslin8@gmail.com>
2025-11-14 15:11:21 +1100

    Lint fix for roomsTabView

--- ios/Rooms/Sources/RoomViews/RoomsTabView.swift
+++ ios/Rooms/Sources/RoomViews/RoomsTabView.swift
@@ -86,13 +86,13 @@ public struct RoomsTabView<Destination: View>: View {
         }
         .opacity(
           roomViewModel.isLoading
-          ? 0
-          : 1) // This hides a glitch where the bottom border of top section row and vice versa flashes when changing order
-        .task {
-          if !roomViewModel.hasLoaded {
-            await roomViewModel.onAppear()
+            ? 0
+            : 1) // This hides a glitch where the bottom border of top section row and vice versa flashes when changing order
+          .task {
+            if !roomViewModel.hasLoaded {
+              await roomViewModel.onAppear()
+            }
           }
-        }
     }
     .navigationTitle("Rooms")
     .searchable(text: $roomViewModel.searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search...")
