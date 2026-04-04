73b4ee479578c25357c65269aaa880cfff0575dd
floormatgen <unwound.podium_0y@icloud.com>
2026-04-05 00:47:28 +1100

    CachedImage now gets cached images in the background

--- ios/CommonUI/Sources/CommonUI/Image/CachedImage.swift
+++ ios/CommonUI/Sources/CommonUI/Image/CachedImage.swift
@@ -13,12 +13,14 @@ public struct CachedImage: View {
     name: String,
     bundle: Bundle,
     size: ImageSize = .medium,
+    cache: ImageCache = .shared,
     fallbackName: String? = nil,
     fallbackBundle: Bundle? = nil)
   {
     self.name = name
     self.bundle = bundle
     self.size = size
+    self.cache = cache
     self.fallbackName = fallbackName
     self.fallbackBundle = fallbackBundle ?? bundle
   }
@@ -26,23 +28,31 @@ public struct CachedImage: View {
   // MARK: Public
 
   public var body: some View {
-    let resolved = ImageCache.shared.image(named: name, in: bundle, size: size)
-      ?? fallbackName.flatMap { ImageCache.shared.image(named: $0, in: fallbackBundle, size: size) }
-
-    if let resolved {
-      Image(uiImage: resolved)
-        .resizable()
-    } else {
-      Rectangle()
-        .fill(.gray.opacity(0.15))
+    Group {
+      if let resolved {
+        Image(uiImage: resolved)
+          .resizable()
+      } else {
+        Rectangle()
+          .fill(.gray.opacity(0.15))
+      }
+    }
+    .task {
+      resolved = await cache.getImage(named: name, in: bundle, size: size)
+      if resolved == nil, let fallbackName {
+        resolved = await cache.getImage(named: fallbackName, in: fallbackBundle, size: size)
+      }
     }
   }
 
   // MARK: Private
+  
+  @State private var resolved: UIImage?
 
   private let name: String
   private let bundle: Bundle
   private let size: ImageSize
+  private let cache: ImageCache
   private let fallbackName: String?
   private let fallbackBundle: Bundle
 }
