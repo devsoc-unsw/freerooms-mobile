c9707395acb70ba43f7bd84752718085acbe0df5
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2026-05-25 17:46:43 +1000

    fix: make sheet no longer transparent

--- ios/Rooms/Sources/RoomViews/RoomDetailsView.swift
+++ ios/Rooms/Sources/RoomViews/RoomDetailsView.swift
@@ -40,6 +40,7 @@ public struct RoomDetailsView: View {
       }
       .presentationDetents([.fraction(0.65), .fraction(0.75), .large], selection: $detent)
       .presentationBackgroundInteraction(.enabled)
+      .presentationBackground(theme.background)
       .presentationCornerRadius(30)
       .interactiveDismissDisabled()
     }
