3bf05c2b9c5fa7917f575f9347fae7a94449ae36
floormatgen <unwound.podium_0y@icloud.com>
2026-05-30 00:25:05 +1000

    Added generated sources to get buildings

---
+++ ios/DevSocAPI/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:6.1
+
+import PackageDescription
+
+let package = Package(
+  name: "DevSocAPI",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v12),
+    .tvOS(.v15),
+    .watchOS(.v8),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(name: "DevSocAPI", targets: ["DevSocAPI"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apollographql/apollo-ios", exact: "2.1.2"),
+  ],
+  targets: [
+    .target(
+      name: "DevSocAPI",
+      dependencies: [
+        .product(name: "ApolloAPI", package: "apollo-ios"),
+      ],
+      path: "./Sources"
+    ),
+  ],
+  swiftLanguageModes: [.v6, .v5]
+)
