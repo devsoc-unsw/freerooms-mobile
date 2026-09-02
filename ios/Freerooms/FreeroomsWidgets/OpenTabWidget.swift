ce0262ce0e862a4e504a57f83a93476970bbc823
Matthew Yuen <unwound.podium_0y@icloud.com>
2026-09-03 02:45:24 +1000

    feat: Added variant system medium widget

--- ios/Freerooms/FreeroomsWidgets/OpenTabWidget.swift
+++ ios/Freerooms/FreeroomsWidgets/OpenTabWidget.swift
@@ -16,21 +16,78 @@ struct OpenTabWidget: Widget {
 
   var body: some WidgetConfiguration {
     StaticConfiguration(kind: kind, provider: EmptyTimelineProvider()) { _ in
-      VStack {
-        Button("Buildings", intent: TabIntents.Buildings())
-        Button("Map", intent: TabIntents.Map())
-        Button("Rooms", intent: TabIntents.Rooms())
-        Button("Bookings", intent: TabIntents.Bookings())
-      }
+      View()
     }
     .configurationDisplayName("Open Tab")
     .description("Convenient way to open a tab")
-    .supportedFamilies([.systemSmall])
+    .supportedFamilies([.systemSmall, .systemMedium])
+  }
+  
+  private struct View: SwiftUI.View {
+    private let theme = Theme.default
+    @Environment(\.widgetFamily) private var family
+    
+    var body: some SwiftUI.View {
+      Grid {
+        GridRow {
+          buildButton("Buildings", for: TabIntents.Buildings(), imageSystemName: "building")
+          buildButton("Map", for: TabIntents.Map(), imageSystemName: "map")
+        }
+        GridRow {
+          buildButton("Rooms", for: TabIntents.Rooms(), imageSystemName: "door.left.hand.open")
+          buildButton("Bookings", for: TabIntents.Bookings(), imageSystemName: "book.closed")
+        }
+      }
+      .buttonBorderShape(.roundedRectangle)
+      .containerBackground(for: .widget) {
+        Color.clear
+      }
+    }
+    
+    @ViewBuilder
+    private func buildButton(
+      _ title: LocalizedStringResource,
+      for intent: some AppIntent,
+      imageSystemName: String
+    ) -> some SwiftUI.View {
+      Group {
+        switch family {
+        case .systemSmall:
+          Button(intent: intent) {
+            Image(systemName: imageSystemName)
+              .resizable()
+              .scaledToFit()
+              .foregroundStyle(theme.accent.primary)
+              .padding(2.0)
+              .frame(maxWidth: .infinity, maxHeight: .infinity) // nasty hack :(
+          }
+        case .systemMedium:
+          Button(intent: intent) {
+            Label(title, systemImage: imageSystemName)
+              .font(.title2)
+              .bold()
+              .foregroundStyle(theme.accent.primary)
+              .frame(maxWidth: .infinity, maxHeight: .infinity)
+          }
+        default:
+          preconditionFailure("\(#function): Unexpected widget family")
+        }
+      }
+      .buttonStyle(.bordered)
+      .tint(theme.accent.secondary)
+    }
+    
   }
 
 }
 
-#Preview(as: .systemSmall) {
+#Preview("System Small", as: .systemSmall) {
+  OpenTabWidget()
+} timeline: {
+  EmptyTimelineProvider.Entry()
+}
+
+#Preview("System Medium", as: .systemMedium) {
   OpenTabWidget()
 } timeline: {
   EmptyTimelineProvider.Entry()
